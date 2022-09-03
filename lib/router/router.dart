4d41455ec913ca3674df0cbec9d19e8db1d75631
amal <amalka@taski.in>
2022-09-02 16:42:05 -1200

    commit

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,17 +1,16 @@
 import 'package:go_router/go_router.dart';
-import 'package:navigation_go_router/Widgets/blah.dart';
+import 'package:navigation_go_router/Widgets/BaseWidget.dart';
 import 'package:navigation_go_router/screens/HomePage.dart';
 import 'package:navigation_go_router/screens/LoginScreen.dart';
 import 'package:navigation_go_router/screens/MainPage.dart';
 import 'package:navigation_go_router/screens/ProfilePage.dart';
-import '../components/BaseWidget.dart';
 
 final routes = GoRouter(
 routes: [
 GoRoute(
   path: '/',
   name: 'mainpage',
-  builder: (context, state) => BaseWidget(),
+  builder: (context, state) => MainPage(),
   ),
   GoRoute(
   path: '/home',
--- lib/screens/SettingsPage.dart
+++ lib/screens/SettingsPage.dart
@@ -2,12 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
+import '../Widgets/BaseWidget.dart';
+
 class SettingsPage extends StatelessWidget {
   const SettingsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text('Settings'),),);
+    return basewidget(Body: Center(child: Text('hello')),color:Colors.amber );
   }
 }

77367372c4a776427cc9ac58dad54acaf048a39b
amal <amalka@taski.in>
2022-09-05 22:27:34 -1200

    commit

--- lib/screens/LoginScreen.dart
+++ lib/screens/LoginScreen.dart
@@ -6,7 +6,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:navigation_go_router/values/styles.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({Key? key}) : super(key: key);
+ TextEditingController _textcontroller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -16,12 +16,18 @@ class LoginScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Login Screen',style:Styles().LoginTextStyle(Colors.deepPurple, 30.0)),
-            SizedBox(height: 30,),
+            TextFormField(
+              controller: _textcontroller,
+            ),
             ElevatedButton(onPressed: () => context.go('/Home'), child: Text('Login'))
           ],
         ),
       ),
     );
   }
+
+  Future<void>SaveData()async{
+    print(_textcontroller.text);
+  }
+
 }
